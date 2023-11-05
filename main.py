7e21acd7fbcd8d04ea34cdcc94ad3e45de898a94
Mona Lisa <imelyashinskiy@gmail.com>
2023-11-05 16:20:20 +0400

    cдеал первый этап

--- main.py
+++ main.py
@@ -3,4 +3,13 @@ from pygame import *
 win_width = 700
 win_height= 500
 display.set_caption("Ping pong")
-window = display
+window = display.set_mode((win_width,win_height))
+back= (200,250,250)
+window.fill(back)
+
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    display.update()
