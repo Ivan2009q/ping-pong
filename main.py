60b8c326f2f9deaa7a795df46f6b3e2f1405967e
Mona Lisa <imelyashinskiy@gmail.com>
2023-11-12 16:29:47 +0400

    ыдвды

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pygame import *
+font.init()
 class GameSprite(sprite.Sprite):
     def __init__(self,player_image, player_x, player_y, player_speed,size_x,size_y):
         super().__init__()
@@ -24,7 +25,8 @@ class Racket(GameSprite):
         if keys[K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
-
+speed_x = 3
+speed_y = 3
 
 
 
@@ -34,19 +36,39 @@ display.set_caption("Ping pong")
 window = display.set_mode((win_width,win_height))
 racket_L = Racket('platform.png',  10,210,5,10,80)
 racket_R = Racket('platform.png',  685,210,5,10,80)
+ball = GameSprite('ball.png',  325,225,5,50,50)
 back= (200,250,250)
 window.fill(back)
 clock= time.Clock()
 FPS=60
 game = True
+finish = False
+font_1 = font.Font(None,35)
+lose1= font_1.render("первый проиграл!",True,(255,0,0))
+lose2= font_1.render("второй проиграл!",True,(255,0,0))
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    window.fill(back)
-    racket_L.update_l()
-    racket_L.reset()
-    racket_R.update_r()
-    racket_R.reset()
+    if not finish:
+        ball.rect.x+= speed_x
+        ball.rect.y+= speed_y
+        if ball.rect.y<0 or ball.rect.y>450:
+            speed_y*=-1
+        if sprite.collide_rect(ball,racket_L) or sprite.collide_rect(ball,racket_R):
+            speed_x*= -1   
+        window.fill(back)
+        racket_L.update_l()
+        racket_L.reset()
+        racket_R.update_r()
+        racket_R.reset()
+        ball.reset()
+        if ball.rect.x< 0: 
+            finish = True 
+            window.blit(lose1,(200,200)) 
+             
+        if ball.rect.x> 700-50: 
+            finish = True 
+            window.blit(lose2,(200,200))  
     display.update()
     clock.tick(FPS)
